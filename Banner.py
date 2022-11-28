24e6b4f813cf63b16b2d6bd854a90fb26d570742
Elise Gielen <elisemgielen@gmail.com>
2022-11-28 14:11:33 +0100

    Added reset button to wipe the grid

--- Banner.py
+++ Banner.py
@@ -6,12 +6,16 @@ class Banner():
         self.display = display
         self.text = init_text
         self.font = get_font(font_size)
-        self.rect = pygame.Rect(0, 0, WIDTH, GRID_START)
+        self.text_surface = self.font.render(self.text, True, FONT_COLOR)
+        self.rect = pygame.Rect(
+            WIDTH / 2 - self.text_surface.get_width()/2 - 5, GRID_START / 2 - self.text_surface.get_height()/2 - 5, 
+            self.text_surface.get_width() + 10, self.text_surface.get_height() + 10)
     
     def print_text(self):
         text_surface = self.font.render(self.text, True, FONT_COLOR)
         self.display.blit(text_surface, 
-        (WIDTH / 2 - text_surface.get_width()/2, GRID_START / 2 - text_surface.get_height()/2))
+            (WIDTH / 2 - text_surface.get_width()/2, 
+            GRID_START / 2 - text_surface.get_height()/2))
 
     def update_text(self, text):
         pygame.draw.rect(self.display, BG_COLOR, self.rect)
